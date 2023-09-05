4a3b5aa1a451dd003b59cba6efca5779b751109d
zakarr <zakariye.arr@xdesign.com>
2023-09-05 17:12:11 +0100

    page cannot go below 1

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,16 +1,29 @@
-"use client"
+'use client';
 import React, { useState } from 'react';
 
-export const Pagination = () => {
-    const [ page, setPage ] = useState<number>(1);
+export const Pagination = ({}) => {
+  const [page, setPage] = useState<number>(1);
 
-    return (
-        <div className='flex content-center my-4 p-4 justify-center'>
-            <div className="join">
-                <button className="join-item btn" onClick={() => setPage(prev => prev - 1)}>«</button>
-                <button className="join-item px-4">Page { page }</button>
-                <button className="join-item btn" onClick={() => setPage(prev => prev + 1)}>»</button>
-            </div>
-        </div>
-    )
+  const nextPage = () => {
+    setPage((prev) => prev + 1);
+  };
+
+  const prevPage = () => {
+    if (page <= 1) return;
+    setPage((prev) => prev - 1);
+  };
+
+  return (
+    <div className='flex content-center my-4 p-4 justify-center'>
+      <div className='join'>
+        <button className='join-item btn' onClick={prevPage}>
+          «
+        </button>
+        <button className='join-item px-4'>Page {page}</button>
+        <button className='join-item btn' onClick={nextPage}>
+          »
+        </button>
+      </div>
+    </div>
+  );
 };
